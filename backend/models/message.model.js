d37c73b6632f45228576fd358de5526b5dc8504d
Silver D <aethermauri@gmail.com>
2024-05-29 11:20:01 -0600

    1st part completed - backend

---
+++ backend/models/message.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiver: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true, // Esto asegura que los campos createdAt y updatedAt se agreguen automáticamente
+  }
+);
+
+const Message = mongoose.model("Message", messageSchema);
+
+export default Message;
